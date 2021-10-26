0020d90fb6db7462d7d3510268ad6ba8fd59906b
debe0025 <deb.jules@outlook.fr>
2021-10-26 18:39:34 +0200

    struggling with typography, home page almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+// core
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// components
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+// metarial
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
