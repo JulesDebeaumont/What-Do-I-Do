1db218f1c537e341550ecd5b2f7e31f501824a74
debe0025 <deb.jules@outlook.fr>
2021-11-01 19:51:59 +0100

    in memory data extends

--- src/app/services/in-memory-data-service.service.ts
+++ src/app/services/in-memory-data-service.service.ts
@@ -1,26 +1,30 @@
-import { HttpClient } from '@angular/common/http';
-import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
-import { Observable } from 'rxjs';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
 
-  constructor(private http: HttpClient) { }
-
   createDb() {
 
-    // Fake table user
     const users = [
       { id: 1, email: 'random@yahoo.fr', password: '1234' },
-      { id: 2, email: 'another@hotmail.fr', password: 'abcd'}
-    ];
+      { id: 2, email: 'another@hotmail.fr', password: 'abcd' }
+    ]
+
+    const tasks = [
+      { id: 1, name: 'Vacuum', activated: false, repeat: 3, start: new Date(), message: 'Vacuum please!', users: 1 },
+      { id: 2, name: 'Dishes', activated: true, repeat: 1, start: new Date(), message: "It's dishes time", users: 1 },
+      { id: 3, name: 'Mayor appointment', activated: false, repeat: 0, start: new Date('2021-11-20'), message: "Don't forgot about the mayor appointment", users: 2 }
+    ]
+
+    const activities = [
+      { id: 1, name: 'Play video-games', duration: 60, users:  2},
+      { id: 1, name: 'Restaurant', duration: 150, users:  2}
+    ]
 
     return {
-      users
+      users,
+      tasks,
+      activities
     };
   }
 
-  // https://github.com/angular/in-memory-web-api#commands
-  resetBd() {
-    this.http.post('command/resetDb', { clear: true })
-  }
 }
