8b3f9b44dd091a80a227d3d1e86ef5b97cd72b50
debe0025 <deb.jules@outlook.fr>
2021-11-01 19:15:00 +0100

    reset in memory data fake bd method

--- src/app/services/in-memory-data-service.service.ts
+++ src/app/services/in-memory-data-service.service.ts
@@ -1,14 +1,26 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { HttpClient } from '@angular/common/http';
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+import { Observable } from 'rxjs';
 
 export class InMemoryDataService implements InMemoryDbService {
+
+  constructor(private http: HttpClient) { }
+
   createDb() {
+
     // Fake table user
     const users = [
-      { id: 1, email: 'random@yahoo.fr', password: '1234' }
+      { id: 1, email: 'random@yahoo.fr', password: '1234' },
+      { id: 2, email: 'another@hotmail.fr', password: 'abcd'}
     ];
 
     return {
       users
     };
+  }
+
+  // https://github.com/angular/in-memory-web-api#commands
+  resetBd() {
+    this.http.post('command/resetDb', { clear: true })
   }
 }
