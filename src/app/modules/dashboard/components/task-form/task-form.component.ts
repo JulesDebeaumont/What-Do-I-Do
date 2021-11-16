424f139cadc9325b4db1e37199fa3deb6db24655
debe0025 <deb.jules@outlook.fr>
2021-11-16 23:59:53 +0100

    get paramter in url task form component

--- src/app/modules/dashboard/components/task-form/task-form.component.ts
+++ src/app/modules/dashboard/components/task-form/task-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskFormComponent implements OnInit {
 
-  constructor() { }
+  taskId: number = Number(this.route.snapshot.paramMap.get('id'));
+  isEdit: boolean = this.taskId !== 0;
+  currentTask?: Task;
+  isLoading: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    if (this.isEdit) {
+      this.getTask(this.taskId);
+    }
+  }
+
+  getTask(taskId: number): void {
+    this.isLoading = true;
+    this.taskService.getTask(taskId)
+      .subscribe((response) => {
+        this.currentTask = response;
+      }, (error) => {
+        console.log(error.message);
+      }, () => {
+        this.isLoading = false;
+      })
+  }
+
+  saveTask(task: Task): void {
+    this.isLoading = true;
+    this.taskService.postTask(task)
+      .subscribe((response) => {
+        this.router.navigateByUrl('/dashboard/tasks');
+      }, (error) => {
+        console.log(error.message);
+      }, () => {
+        this.isLoading = false;
+      })
   }
 
 }
