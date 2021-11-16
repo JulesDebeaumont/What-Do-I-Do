c45279e5eae1178d010e0de0d64eb6108657e27b
debe0025 <deb.jules@outlook.fr>
2021-11-16 17:33:31 +0100

    tasks service update with fixed routes

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,6 +13,7 @@ import { AuthService } from './auth.service';
 })
 export class TaskService {
 
+  private apiTasksUrl: string = '/tasks';
   private httpOptions: object = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
@@ -29,38 +30,38 @@ export class TaskService {
    * GET - Get tasks by user
    */
   getAllUserTasks(): Observable<any> {
-    return this.http.get<any>(environment.apiUrl + `/users/${this.auth.getUserId()}/tasks`, this.httpOptions);
+    return this.http.get<any>(environment.apiUrl + `/users/${this.auth.getUserId()}${this.apiTasksUrl}`, this.httpOptions);
   }
 
 
   /**
   * GET - Get task by user
   */
-  getUserTask(taskId: number): Observable<any> {
-    return this.http.get<any>(environment.apiUrl + `/users/${this.auth.getUserId()}/tasks/${taskId}`, this.httpOptions);
+  getTask(taskId: number): Observable<any> {
+    return this.http.get<any>(environment.apiUrl + `${this.apiTasksUrl}/${taskId}`, this.httpOptions);
   }
 
 
   /**
    * POST - Post task by user
    */
-  postUserTask(taskData: Task): Observable<any> {
-    return this.http.post<any>(environment.apiUrl + `/users/${this.auth.getUserId()}/tasks/${taskData.id}`, taskData, this.httpOptions);
+  postTask(taskData: Task): Observable<any> {
+    return this.http.post<any>(environment.apiUrl + `${this.apiTasksUrl}/${taskData.id}`, taskData, this.httpOptions);
   }
 
 
   /**
   * PATCH - Patch task by user
   */
-  patchUserTask(taskData: Task): Observable<any> {
-    return this.http.patch<any>(environment.apiUrl + `/users/${this.auth.getUserId()}/tasks/${taskData.id}`, taskData, this.httpOptions);
+  patchTask(taskData: Task): Observable<any> {
+    return this.http.patch<any>(environment.apiUrl + `${this.apiTasksUrl}/${taskData.id}`, taskData, this.httpOptions);
   }
 
 
   /**
   * DELETE - Delete task by user
   */
-  deleteUserTask(taskId: number): Observable<any> {
-    return this.http.delete<any>(environment.apiUrl + `/users/${this.auth.getUserId()}/tasks/${taskId}`, this.httpOptions);
+  deleteTask(taskId: number): Observable<any> {
+    return this.http.delete<any>(environment.apiUrl + `${this.apiTasksUrl}/${taskId}`, this.httpOptions);
   }
 }
